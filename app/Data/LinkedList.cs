a67c4af91100a3a5f1eb2e875aec698ee55ae3b3
Ivan Chensky <ichensky@live.com>
2020-06-22 23:46:50 +0300

    add fix

---
+++ app/Data/LinkedList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Xml;
+
+namespace Data
+{
+    public class Node<T> {
+        internal SinglyLinkedList<T> List;
+        internal Node<T> Next;
+        //public Node<T> Prev;
+        public T Item;
+        
+        public Node(T item) {
+            Item = item;
+        }
+        internal Node(SinglyLinkedList<T> list, T item) => (List, Item) = (list, item);
+    }
+   
+    public class SinglyLinkedList<T>
+    {
+        public Node<T> first;
+        public Node<T> last;
+
+        public Node<T> Add(T item)
+        {
+            var node = new Node<T>(this,item);
+            if (last==null)
+            {
+                first = node;
+                last = node;
+                return last;
+            }
+
+            last.Next = node;
+            last = node;
+            return node;
+        }
+
+        public Node<T> Contains(T value) {
+            if (first==null)
+            {
+                return null;
+            }
+            Node<T> node = first;
+            do
+            {
+                if (EqualityComparer<T>.Default.Equals(node.Item, value))
+                {
+                    return node;
+                }
+                node = node.Next;
+            }
+            while (node != null);
+            return null;
+        }
+        public void Delete(Node<T> n)
+        {
+            ValidateNode(n);
+            Node<T> node = first;
+            Node<T> prev = null;
+            do
+            {
+                if (node == n)
+                {
+                    break;
+                }
+                prev = node;
+                node = node.Next;
+            }
+            while (node != null);
+            if (prev == null)
+            {
+                first = first.Next;
+            }
+            else {
+                prev.Next = n.Next;
+                if (n.Next==null)
+                {
+                    last = prev;
+                }
+            }
+        }
+
+
+        internal void ValidateNode(Node<T> node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException(nameof(node));
+            }
+
+            if (node.List != this)
+            {
+                throw new InvalidOperationException("Externale linked list node.");
+            }
+        }
+    }
+}
---
+++ app/app/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace app
+{
+    class Program
+    {
+        class A{}
+        static void Main(string[] args)
+        {
+            var list = new Data.SinglyLinkedList<string>();
+            var node = list.Add("xxx");
+            list.Add("yyy");
+            list.Add("zzz");
+
+
+            list.Delete(node);
+
+            //var r =list.Contains("yyy");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
